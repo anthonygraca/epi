74844941add96de3a68ed5b1de8d7902880406da
Anthony Graca <anthonygraca@gmail.com>
2021-10-23 08:44:05 -0700

    basic setup of Counting Bits

---
+++ main/data-structures-and-algorithms/primitive-types/counting-bits.h
@@ -0,0 +1,15 @@
+#ifndef ELEMENTS_OF_PROGRAMMING_INTERVIEWS_DATA_STRUCTURES_AND_ALGORITHMS_PRIMITIVE_TYPES_COUNTING_BITS_H_
+#define ELEMENTS_OF_PROGRAMMING_INTERVIEWS_DATA_STRUCTURES_AND_ALGORITHMS_PRIMITIVE_TYPES_COUNTING_BITS_H_
+
+#include <vector>
+
+namespace epi {
+class CountingBits {
+public:
+  std::vector<int> countBits(int n) {
+    return {0,1,1};
+  }
+};
+} // namespace epi
+
+#endif
---
+++ test/data-structures-and-algorithms/primitive-types/counting-bits-test.cc
@@ -0,0 +1,19 @@
+#include "main/data-structures-and-algorithms/primitive-types/counting-bits.h"
+
+#include "gtest/gtest.h"
+
+#include <vector>
+
+TEST(CountingBits, FirstExample) {
+  int n{2};
+  epi::CountingBits algo;
+  std::vector<int> expected{0,1,1};
+  ASSERT_EQ(expected, algo.countBits(n));
+}
+
+TEST(CountingBits, DISABLED_SecondExample) {
+  int n{5};
+  epi::CountingBits algo;
+  std::vector<int> expected{0,1,1,2,1,2};
+  ASSERT_EQ(expected, algo.countBits(n));
+}
