b8e1c321d650c73c0da2a6e6ef27817491817342
Anthony Graca <anthonygraca@gmail.com>
2021-11-11 22:52:39 -0800

    added infrastructure for Compute Pow

---
+++ src/main/data-structures-and-algorithms/primitive-types/compute-pow.h
@@ -0,0 +1,13 @@
+#ifndef EPI_SRC_MAIN_DATA_STRUCTURES_AND_ALGORITHMS_PRIMITIVE_TYPES_COMPUTE_POW_H_
+#define EPI_SRC_MAIN_DATA_STRUCTURES_AND_ALGORITHMS_PRIMITIVE_TYPES_COMPUTE_POW_H_
+
+namespace epi {
+class ComputePow {
+public:
+  int pow(int x, int y) {
+    return 8;
+  }
+};
+} // namespace epi
+
+#endif
---
+++ src/test/data-structures-and-algorithms/primitive-types/compute-pow-test.cc
@@ -0,0 +1,8 @@
+#include "src/main/data-structures-and-algorithms/primitive-types/compute-pow.h"
+
+#include "gtest/gtest.h"
+
+TEST(ComputePow, FirstExample) {
+  epi::ComputePow algo;
+  ASSERT_EQ(8, algo.pow(2,3));
+}
