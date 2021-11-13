24500556c510f880ccb3493f9ba8a6da68101fc6
Anthony Graca <anthonygraca@gmail.com>
2021-11-13 14:33:29 -0800

    implemented faster solution for counting bits. added more tests for product

--- src/main/data-structures-and-algorithms/primitive-types/counting-bits.h
+++ src/main/data-structures-and-algorithms/primitive-types/counting-bits.h
@@ -12,9 +12,9 @@ class CountingBits {
 public:
   int countBits(uint32_t n) {
     int count{0};
-    while (n >= 1) {
-      count += n & 1;
-      n = n >> 1;
+    while (n >= 1) { // the runtime is O(k), where k == number of 1s
+      count += 1;
+      n = n & (n-1);
     }
     return count;
   }
--- src/test/data-structures-and-algorithms/primitive-types/product-without-arithmetical-operators-test.cc
+++ src/test/data-structures-and-algorithms/primitive-types/product-without-arithmetical-operators-test.cc
@@ -6,3 +6,28 @@ TEST(ProductWithoutArithmeticalOperators, FirstExample) {
   epi::ProductWithoutArithmeticalOperators algo;
   ASSERT_EQ(6, algo.product(2,3));
 }
+
+TEST(ProductWithoutArithmeticalOperators, SecondExample) {
+  epi::ProductWithoutArithmeticalOperators algo;
+  ASSERT_EQ(256, algo.product(16,16));
+}
+
+TEST(ProductWithoutArithmeticalOperators, SecondExampleInBinary) {
+  epi::ProductWithoutArithmeticalOperators algo;
+  ASSERT_EQ(0b100000000, algo.product(0b10000,0b10000));
+}
+
+TEST(ProductWithoutArithmeticalOperators, ThirdExample) {
+  epi::ProductWithoutArithmeticalOperators algo;
+  ASSERT_EQ(240, algo.product(16,15));
+}
+
+TEST(ProductWithoutArithmeticalOperators, ThirdExampleInBinary) {
+  epi::ProductWithoutArithmeticalOperators algo;
+  ASSERT_EQ(0b11110000, algo.product(0b10000,0b1111));
+}
+
+TEST(ProductWithoutArithmeticalOperators, FourthExample) {
+  epi::ProductWithoutArithmeticalOperators algo;
+  ASSERT_EQ(117, algo.product(13,9));
+}
